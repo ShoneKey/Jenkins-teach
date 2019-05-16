8bd2f280ebc6de460398177c1cb600033d0c0b8e
Shone <shonekey@163.com>
2019-05-16 16:24:17 +0800

    add jenkinsdir

---
+++ Jenkinsfile/src/org/foo/Bar.groovy
@@ -0,0 +1,13 @@
+
+    stage('Build'){
+        checkout scm
+        echo 'start build'
+    }
+    stage('Test'){
+        checkout scm
+        echo 'start test'
+    }
+    stage('Deploy'){
+        checkout scm
+        echo 'start deploy'
+    }
