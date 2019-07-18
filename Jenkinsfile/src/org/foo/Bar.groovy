479115c09aa0fd645651893fd88925c2242166ce
shonekey <shonkey@163.com>
2019-07-18 23:27:41 +0800

    local lib test

--- Jenkinsfile/src/org/foo/Bar.groovy
+++ Jenkinsfile/src/org/foo/Bar.groovy
@@ -1,13 +1,15 @@
+package org.foo.Bar
 
+def start(){
     stage('Build'){
-        checkout scm
         echo 'start build'
     }
     stage('Test'){
-        checkout scm
         echo 'start test'
     }
     stage('Deploy'){
-        checkout scm
         echo 'start deploy'
     }
+}
+
+return this
